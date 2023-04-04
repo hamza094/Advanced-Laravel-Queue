dd7e4a998b8ebf4e7a755ab05dd51f77cb6068db
hamza094 <hamza_pisces@live.com>
2023-04-04 05:48:17 +0500

    theory

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        /*In an application that's built using event-driven programming, a NewOrderSubmitted event has several listeners:*/
+        
     ];
 
     /**
